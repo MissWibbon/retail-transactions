10c092c0af0b71da494952f069150d2ddccb10ed
Michelle <35238012+MissWibbon@users.noreply.github.com>
2022-02-15 13:19:02 -0600

    added data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import { conversionsJanFebMarch } from './data/retail-converstions_jan-march_2020';
+import axios from 'axios';
 
-function App() {
+
+
+function App(props) {
+  const [loading, setLoading] = useState(false);
+  const [conversions, setConversions] = useState([]);
+  const distinct = []
+  const [customers, setCustomers] = useState({})
+  const key = 'Name';
+
+  
+  useEffect(() => {
+    const loadPost = async () => {
+      
+      // Till the data is fetch using API 
+      // the Loading page will show.
+      setLoading(true);
+      const arrayUniqueByKey = [...new Map(conversionsJanFebMarch.map(item =>
+        [item[key], item])).values()];
+
+        // Await make wait until that 
+        // promise settles and return its reult
+        const response = await axios.get(conversionsJanFebMarch);
+
+        // After fetching data stored it in posts state.
+        setConversions(response.data);
+
+        // Closed the loading page
+        setLoading(false);
+    }
+
+    // Call the function
+    loadPost();
+}, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {loading ? (
+          <h4>Loading...</h4>) :
+          (conversions.map((item) =>
+              // Presently we only fetch 
+              // title from the API 
+              <h4>{item.Name}</h4>)
+          )
+      }
     </div>
   );
 }
---
+++ src/Data.jsx
@@ -0,0 +1,10 @@
+import React, { useContext, useEffect } from 'react'
+
+
+const Data = (props) => {
+
+    return (
+        <div>Data</div>
+    )
+}
+export default Data
